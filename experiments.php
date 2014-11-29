567289fe46e2f49de3dbdee2fbaf424b762b2b26
James George Dunn <james@jamesgeorgedunn.com>
2014-11-29 22:12:21 +0000

    Initial

---
+++ experiments.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Freelance web design in Newcastle upon Tyne. Front end design and build.">
+	<meta name="author" content="James George Dunn">
+	<title>
+		Freelance Web Design and Development - James George Dunn
+	</title>
+	<link href="css/styles.css" media="screen" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic,700' rel='stylesheet' type='text/css'>
+	<script src="js/masonry.pkgd.min.js"></script>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-8178078-1']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="row-1">
+			<header>
+				<a href="<?php $url="http://".$_SERVER['HTTP_HOST']; echo $url; ?>">
+					<div class="dt-2 tl-2 tp-4">
+						<div class="inner">
+							Home
+						</div>
+					</div>
+				</a>
+				<a href="https://twitter.com/jamesgeorgedunn">
+					<div class="dt-5 tl-5 tp-4">
+						<div class="inner">
+							<i class="fa fa-twitter"></i>Follow Me
+						</div>
+					</div>
+				</a>
+				<a href="mailto:james@jamesgeorgedunn.com">
+					<div class="dt-5 tl-5 tp-4">
+						<div class="inner">
+							<i class="fa fa-envelope"></i>Let's Talk
+						</div>
+					</div>
+				</a>
+			</header>
+		</div>
+		<div class="row-10">
+			<div class="dt-12 tl-12 tp-12">
+				<h1>Experiments &amp; Snippets</h1>
+			</div>
+			<div class="dt-2"></div>
+			<div class="dt-8 tl-12 tp-12">
+				<h2>Here you can find some of my coding projects and little snippets that I find to be very useful on a day to day basis.</h2>
+				<h3>I hope that you do too.</h3>
+			</div>
+			<div class="dt-2"></div>
+			<div class="experiments js-masonry">
+				<div class="dt-4 tl-4 tp-6 ml-12">
+					<div class="row">
+						<div class="inner">
+							<h3><i class="fa fa-folder"></i>Modest Grid</h3>
+							<p>Modest Grid is a super lightweight grid system developed to create responsive prototypes. With the CSS weighing in at under 2kB, it truly is a super lightweight.</p>
+							<p>What really sets Modest Grid apart are its naming conventions for the column classes it uses. As well as the ability to choose how many columns you would like to use for your projects.</p>
+							<a href="https://github.com/jamesgeorgedunn/Modest-Grid">
+								<div class="download">
+									Download Now
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="dt-4 tl-4 tp-6 ml-12">
+					<div class="row">
+						<div class="inner">
+							<h3><i class="fa fa-code"></i>CSS Reset</h3>
+							<p>Modest Size is a simple CSS reset based on the CSS reset framework by Eric Meyer.</p>
+							<p>It has additional resets that I find very useful when starting a new project.</p>
+							<p>These include:</p>
+							<p>No marching ants around link elements.</p>
+							<p>Textareas that don't resize with user interaction.</p>
+							<p>Vertical scrollbar is always present.</p>
+							<a href="https://github.com/jamesgeorgedunn/Modest-Reset">
+								<div class="download">
+									Download Now
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="dt-4 tl-4 tp-6 ml-12">
+					<div class="row">
+						<div class="inner">
+							<h3><i class="fa fa-code"></i>Browser Size</h3>
+							<p>I developed this browser size applet to help me to determine the size of a browser window.</p>
+							<p>Many modern browsers have this functionality built into them but it was mainly for use in older browsers.</p>
+							<a href="https://github.com/jamesgeorgedunn/Modest-Size">
+								<div class="download">
+									Download Now
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="dt-4 tl-4 tp-6 ml-12">
+					<div class="row">
+						<div class="inner">
+							<h3><i class="fa fa-code"></i>Back Or Load</h3>
+							<p>I like using this code when it comes to WordPress blogs. It comes in useful when you would like to add a back button to help the user keep their position on the previous page. But if the user say came from another link, then they will just be returned to the top of the page.</p>
+							<a href="https://github.com/jamesgeorgedunn/Back-Or-Load">
+								<div class="download">
+									Download Now
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row-1">
+			<footer>
+				<p>Developed using <a href="http://www.modestgrid.com" target="_blank">Modest Grid</a>.</p>
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Freelance web design in Newcastle upon Tyne. Front end design and build.">
+	<meta name="author" content="James George Dunn">
+	<title>
+		Freelance Web Design and Development - James George Dunn
+	</title>
+	<link href="css/styles.css" media="screen" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic,700' rel='stylesheet' type='text/css'>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-8178078-1']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="row-1">
+			<header>
+				<a href="<?php $url="http://".$_SERVER['HTTP_HOST']; echo $url; ?>">
+					<div class="dt-2 tl-2 tp-4">
+						<div class="inner">
+							Home
+						</div>
+					</div>
+				</a>
+				<a href="https://twitter.com/jamesgeorgedunn">
+					<div class="dt-5 tl-5 tp-4">
+						<div class="inner">
+							<i class="fa fa-twitter"></i>Follow Me
+						</div>
+					</div>
+				</a>
+				<a href="mailto:james@jamesgeorgedunn.com">
+					<div class="dt-5 tl-5 tp-4">
+						<div class="inner">
+							<i class="fa fa-envelope"></i>Let's Talk
+						</div>
+					</div>
+				</a>
+			</header>
+		</div>
+		<div class="row-10">
+			<div class="dt-12 tl-6 tp-12">
+				<h1>James George Dunn</h1>
+			</div>
+			<div class="dt-12 tl-6 tp-12">
+				<h2>Freelance website design and web development</h2>
+				<h3>In the heart of Newcastle upon Tyne.</h3>
+				<p>A marriage between pixel perfect design and development.</p>
+			</div>
+			<div class="dt-4"></div>
+			<div class="dt-4">
+				<a href="<?php $url="http://".$_SERVER['HTTP_HOST']; echo $url; ?>/experiments.php">
+					<div class="contact">
+						<i class="fa fa-code"></i>Experiments and Snippets
+					</div>
+				</a>
+			</div>
+			<div class="dt-4"></div>
+		</div>
+		<div class="row-1">
+			<footer>
+				<p>Developed using <a href="http://www.modestgrid.com" target="_blank">Modest Grid</a>.</p>
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
